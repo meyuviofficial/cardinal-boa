109862a12552a444090e9cd7c69b6562bc15c0d1
Yuvi <yuvarajselva@hotmail.com>
2024-03-07 01:46:40 +0530

    added the base html

--- cardinalboa/cardinalboa/urls.py
+++ cardinalboa/cardinalboa/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.views.generic import TemplateView
 from django.urls import path, include
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='index.html'), name='home' ),
     path('users/', include("users.urls") ),
     path('admin/', admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
