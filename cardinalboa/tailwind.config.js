b2cf12ae4cf55f510f09c74aeda7d913ce4f9930
Yuvi <yuvarajselva@hotmail.com>
2024-03-07 01:34:28 +0530

    configured the tailwind

---
+++ cardinalboa/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./users/templates/users/**/*.html"],
+  // Add paths to apps that are built in the future
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
